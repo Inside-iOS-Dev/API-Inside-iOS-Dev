6a124e591fd371e00e32e51c5747bce5c7ea838a
Alex Bush <alex.v.bush@gmail.com>
2022-03-13 10:21:37 -0500

    Another commit that should auto deploy

--- Sources/App/Controllers/PostsController.swift
+++ Sources/App/Controllers/PostsController.swift
@@ -2,7 +2,9 @@ import Vapor
 
 final class PostsController {
     func index(_ request: Request) throws -> Future<Response> {
-        let posts = [Post(id: 1, title: "First Post!"), Post(id: 2, title: "second post title")]
+        let posts = [Post(id: 1, title: "First Post!"),
+                     Post(id: 2, title: "second post title"),
+                     Post(id: 3, title: "3rd Post")]
         let response = PostsResponse(posts: posts)
         return response.encode(status: .created, for: request)
     }
