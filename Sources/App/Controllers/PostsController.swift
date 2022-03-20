72bc8f0d969ff77c9d047b77c65dec0be3e356f2
Alex Bush <alex.v.bush@gmail.com>
2022-03-20 11:16:18 -0500

    Add summary

--- Sources/App/Controllers/PostsController.swift
+++ Sources/App/Controllers/PostsController.swift
@@ -2,10 +2,10 @@ import Vapor
 
 final class PostsController {
     func index(_ request: Request) throws -> Future<Response> {
-        let posts = [Post(id: "1", title: "Hello", imageURL: "https://loremflickr.com/320/240"),
-                     Post(id: "2", title: "This", imageURL: "https://loremflickr.com/320/240/dog"),
-                     Post(id: "3", title: "From ", imageURL: "https://loremflickr.com/g/320/240/paris"),
-                     Post(id: "4", title: "The Backend", imageURL: "https://image.simplecastcdn.com/images/80304120-6f39-4345-8f8c-7e654e5ede07/a8c50ee6-0513-4e42-be32-abacacec2ab3/640x640/1493514588artwork.jpg")]
+        let posts = [Post(id: "1", title: "Hello", imageURL: "https://loremflickr.com/320/240", summary: "This is backend summary"),
+                     Post(id: "2", title: "This", imageURL: "https://loremflickr.com/320/240/dog", summary: "This is backend summary"),
+                     Post(id: "3", title: "From ", imageURL: "https://loremflickr.com/g/320/240/paris", summary: "This is backend summary"),
+                     Post(id: "4", title: "The Backend", imageURL: "https://image.simplecastcdn.com/images/80304120-6f39-4345-8f8c-7e654e5ede07/a8c50ee6-0513-4e42-be32-abacacec2ab3/640x640/1493514588artwork.jpg", summary: "This is backend summary")]
         let response = PostsResponse(posts: posts)
         return response.encode(status: .created, for: request)
     }
@@ -19,4 +19,5 @@ struct Post: Content {
     let id: String
     let title: String
     let imageURL: String
+    let summary: String
 }
