a8dc294225baf40f6ee1b421b6c80b060a1b8600
Alex Bush <alex.v.bush@gmail.com>
2022-03-20 09:15:53 -0500

    Add image urls to posts

--- Sources/App/Controllers/PostsController.swift
+++ Sources/App/Controllers/PostsController.swift
@@ -2,9 +2,10 @@ import Vapor
 
 final class PostsController {
     func index(_ request: Request) throws -> Future<Response> {
-        let posts = [Post(id: 1, title: "First Post!"),
-                     Post(id: 2, title: "second post title"),
-                     Post(id: 3, title: "3rd Post")]
+        let posts = [Post(id: "1", title: "Hello", imageURL: "https://loremflickr.com/320/240"),
+                     Post(id: "2", title: "Guys", imageURL: "https://loremflickr.com/320/240/dog"),
+                     Post(id: "3", title: "This is ", imageURL: "https://loremflickr.com/g/320/240/paris"),
+                     Post(id: "4", title: "Inside ios dev", imageURL: "https://loremflickr.com/320/240/brazil,rio")]
         let response = PostsResponse(posts: posts)
         return response.encode(status: .created, for: request)
     }
@@ -15,6 +16,7 @@ struct PostsResponse: Content {
 }
 
 struct Post: Content {
-    let id: Int
+    let id: String
     let title: String
+    let imageURL: String
 }
